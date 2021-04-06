0b982bf51fb5b46848b41a4a97428048ae8c6e31
GWeindel <gabriel.weindel@gmail.com>
2021-04-06 11:10:35 +0200

    First commit.

---
+++ content/posts/presentation.md
@@ -0,0 +1,10 @@
+Title: Introducing myself
+Date: 2020-04-06 10:20
+Modified: 2020-04-06 10:20
+Category: 
+Tags: CV, introduction
+Slug: introduction
+Authors: Gabriel Weindel
+Summary: 
+
+This is where I am going to introduce myself
---
+++ pelicanconf.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Gabriel Weindel'
+SITENAME = 'Personal Website'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+THEME = 'Flex'
+
+# --------------8<---------------------
+# Files and content
+
+
+# This will look for a directory img/ 
+# inside the directory content/
+# The contents of img/ will be available at 
+# {{ SITEURL }}/img
+STATIC_PATHS = ['img']
+
+# If we want to create static pages,
+# we should put them in content/pages
+PAGE_PATHS = ['pages']
+
+# If we want to create blog posts (articles),
+# we should put them in content/posts
+ARTICLE_PATHS = ['posts']
+
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
